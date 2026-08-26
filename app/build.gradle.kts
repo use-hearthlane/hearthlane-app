dc331c63610fc40d48f7950b1e74295718e1e863
braz <me@mrbraz.net>
2026-08-26 19:46:42 +0000

    chore: bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.homelab.hearthlane"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         buildConfigField("String", "FRIGATE_BASE_URL", "\"$frigateBaseUrl\"")
     }
